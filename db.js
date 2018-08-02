cdfdc2227abd0461ed96c8cb674d29b5cdc39f30
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-08-02 13:09:31 +0530

    create database connection with mongoose

---
+++ db.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect('mongodb://localhost:31000/' + 'test?replicaSet=rs0')
+      .then(() => {
+        console.log('Database connection successful');
+      })
+      .catch(err => {
+        console.error('Database connection error', err);
+      });
+  }
+}
+
+module.exports = new Database();
