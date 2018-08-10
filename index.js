b1d50bcce42257460dccc98b08e0d18ff82fa810
Akhil Mohammed <mohammed.akhil@gyanmatrix.com>
2018-08-10 09:19:02 +0530

    modify index.js with new schema

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const app = require('express')(),
-  http = require('http').Server(app),
-  io = require('socket.io')(http),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose');
+  express = require('express');
+(http = require('http').Server(app)),
+  (io = require('socket.io')(http)),
+  (port = process.env.PORT || 3000),
+  (mongoose = require('mongoose'));
 require('./db');
-const { SuperHero } = require('./superhero/');
-new SuperHero(io);
+
+const { Graph } = require('./graph/');
+new Graph(io);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/insert', (req, res) => {
+  res.sendFile(__dirname + '/insert.html');
 });
 
 http.listen(port, function() {
